06d2670b3499126a322c1aaee9880ba02edcf0e1
Razine Bensari <bensaria97@gmail.com>
2020-03-28 13:17:41 -0400

    implemented unigram with vocabulary 0 and 1

--- Main.py
+++ Main.py
@@ -4,6 +4,11 @@ Author: Annes Cherrid, cherid.annes@gmail.com
 """
 from NaiveBayesClassifier import NaiveBayesClassifier
 
-model = NaiveBayesClassifier(0, 1, 0, "./training_files/gl_training-tweets.txt", "empty.txt")
+model = NaiveBayesClassifier(1, 1, 0, "./training_files/gl_training-tweets.txt", "gl", 18318)
 
 model.constructModel()
+
+prob_base10 = model.calculateProbability("@AnderDelPozo @PesqueWhite hahaha yo tambien me he quedao pillao ahahha")
+
+print(str(prob_base10) + "for tweet: @AnderDelPozo @PesqueWhite hahaha yo tambien me he quedao pillao ahahha which is "
+                         "es")
--- NaiveBayesClassifier.py
+++ NaiveBayesClassifier.py
@@ -5,16 +5,18 @@ https://scikit-learn.org/stable/auto_examples/model_selection/plot_precision_rec
 """
 import numpy as np
 import string
+import math
 
 
 def getLanguage(array):
     return array[0]
 
 
-def getFrequencies(UV0, tweet):
+# BOW is a Bag of Word dictionary using alphabet as keys and frequencies as values
+def getFrequencies(BOW, tweet):
     for c in tweet:
-        if c in UV0:
-            UV0[c] += 1
+        if c in BOW:
+            BOW[c] += 1
 
 
 def buildUnigramsWhenVocabularyIsOne():
@@ -49,31 +51,31 @@ def buildTrigramsWhenVocabularyIsTwo():
     None
 
 
-def addSmoothing(BOW_V0, delta):
-    for c in BOW_V0:
-        if BOW_V0[c] == 0:
-            BOW_V0[c] += delta
+# BOW is a Bag of Word dictionary using alphabet as keys and frequencies as values
+def addSmoothing(BOW, delta):
+    for c in BOW:
+        if BOW[c] == 0:
+            BOW[c] += delta
 
 
 class NaiveBayesClassifier:
-    def __init__(self, vocabulary, n, delta, trainingFile, testingFile):
+    def __init__(self, vocabulary, n, delta, trainingFile, language, totalTweetCount):
         self.typeOfNGrams = None
         self.vocabulary = vocabulary
         self.n = n
         self.delta = delta
         self.trainingFile = trainingFile
-        self.testingFile = testingFile
         self.array = None
         self.BOW_V0 = dict.fromkeys("abcdefghijklmnopqrstuvwxyz", 0)
         self.BOW_V1 = dict.fromkeys("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", 0)
         self.BOW_V2 = dict()
-        self.language = ""
+        self.language = language
         self.probability = 0
         self.tweetCount = 0
+        self.totalTweetCount = totalTweetCount
 
     def constructModel(self):
         self.buildNGrams()
-        # self.testModel()
 
     def buildNGrams(self):
         if self.n == 1:
@@ -90,7 +92,7 @@ class NaiveBayesClassifier:
         if self.vocabulary == 0:
             self.buildUnigramsWhenVocabularyIsZero()
         if self.vocabulary == 1:
-            buildUnigramsWhenVocabularyIsOne()
+            self.buildUnigramsWhenVocabularyIsOne()
         if self.vocabulary == 2:
             buildUnigramsWhenVocabularyIsTwo()
 
@@ -122,3 +124,77 @@ class NaiveBayesClassifier:
         print("Tweet count: " + str(tweetCount))
         print(self.BOW_V0)
         addSmoothing(self.BOW_V0, self.delta)
+
+    def buildUnigramsWhenVocabularyIsOne(self):
+        self.language = getLanguage(self.trainingFile.split("_"))
+        tweetCount = 0
+        with open(self.trainingFile, "r") as file:
+            for line in file:
+                tweetArray = line.split("\t")
+                getFrequencies(self.BOW_V1, tweetArray[3])
+                tweetCount += 1
+        self.tweetCount = tweetCount
+        print("Tweet count: " + str(tweetCount))
+        print(self.BOW_V1)
+        addSmoothing(self.BOW_V1, self.delta)
+
+    def calculateProbability(self, tweet):
+        if self.n == 1:
+            if self.vocabulary == 0:
+                return self.calculate_probability_n1_v0(tweet)
+            elif self.vocabulary == 1:
+                return self.calculate_probability_n1_v1(tweet)
+            elif self.vocabulary == 2:
+                pass
+            else:
+                print("Invalid V")
+        elif self.n == 2:
+            if self.vocabulary == 0:
+                pass
+            elif self.vocabulary == 1:
+                pass
+            elif self.vocabulary == 2:
+                pass
+            else:
+                print("Invalid V")
+        elif self.n == 3:
+            if self.vocabulary == 0:
+                pass
+            elif self.vocabulary == 1:
+                pass
+            elif self.vocabulary == 2:
+                pass
+            else:
+                print("Invalid V")
+        else:
+            print("Invalid n")
+
+    def calculate_probability_n1_v0(self, tweet):
+        sum_of_prob = 0
+        total_chars = sum(self.BOW_V0.values())
+        print("total char in bow: " + str(total_chars))
+        tweet_ratio = self.tweetCount / self.totalTweetCount
+        print("Tweet probablity: " + str(tweet_ratio))
+        prob_of_tweet_base10 = math.log(tweet_ratio, 10)
+        for c in tweet:
+            if c in self.BOW_V0:
+                char_ratio = self.BOW_V0.get(c) / total_chars
+                sum_of_prob += math.log(char_ratio, 10)
+                print("char probablity: " + str(self.BOW_V0.get(c) / total_chars))
+        self.probability = prob_of_tweet_base10 + sum_of_prob
+        return self.probability
+
+    def calculate_probability_n1_v1(self, tweet):
+        sum_of_prob = 0
+        total_chars = sum(self.BOW_V1.values())
+        print("total char in bow: " + str(total_chars))
+        tweet_ratio = self.tweetCount / self.totalTweetCount
+        print("Tweet probablity: " + str(tweet_ratio))
+        prob_of_tweet_base10 = math.log(tweet_ratio, 10)
+        for c in tweet:
+            if c in self.BOW_V1:
+                char_ratio = self.BOW_V1.get(c) / total_chars
+                sum_of_prob += math.log(char_ratio, 10)
+                print("char probablity: " + str(self.BOW_V1.get(c) / total_chars))
+        self.probability = prob_of_tweet_base10 + sum_of_prob
+        return self.probability
